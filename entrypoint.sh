2402bae93af6e14a502c3bbe7745b208651f8f3a
yun geun yong <yungeunyong@yunui-MacBookPro.local>
2022-06-02 16:22:04 +0900

    2

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,17 @@
 #!/bin/sh -l
 
-echo "Hello $1"
-time=$(date)
-echo "::set-output name=time::$time"
+sudo apt update
+sudo apt install -y software-properties-common
+sudo add-apt-repository ppa:ethereum/ethereum
+sudo apt install -y solc
+sudo apt install -y libssl-dev python3-dev python3-pip
+yes | pip3 install mythril
+myth version
+myth analyze *.sol -o json
+
+
+
+
+#echo "Hello $1"
+#time=$(date)
+#echo "::set-output name=time::$time"
