60ee559692dfb0da101c86a417b323417524443a
yun geun yong <yungeunyong@yunui-MacBookPro.local>
2022-06-02 18:22:14 +0900

    6

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,15 +1,15 @@
 #!/bin/sh -l
 
-apt update
-apt install -y software-properties-common
-add-apt-repository ppa:ethereum/ethereum
-apt install -y solc
-apt install -y libssl-dev python3-dev python3-pip
-yes | pip3 install mythril
-myth version
-myth analyze calls.sol -o json
+#apt update
+#apt install -y software-properties-common
+#add-apt-repository ppa:ethereum/ethereum
+#apt install -y solc
+#apt install -y libssl-dev python3-dev python3-pip
+#yes | pip3 install mythril
+#myth version
+#myth analyze calls.sol -o json
 
-
+ls -R
 
 
 #echo "Hello $1"
