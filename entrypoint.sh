db2611660a0977e995f44054964fc218b06d6c3b
yun geun yong <yungeunyong@yunui-MacBookPro.local>
2022-06-08 17:47:06 +0900

    9

--- entrypoint.sh
+++ entrypoint.sh
@@ -11,34 +11,17 @@ apt install -y solc
 
 #
 # Mythrilp
+#
 #yes | pip3 install mythril
 #myth version
 #myth analyze /github/workspace/*.sol -o json
 
-#ls -al /
-#ls -R /github
-#ls -R /home
-#ls -R /mnt
-#ls -R /run
-#ls -R /tmp
-
-#echo "Hello $1"
-#time=$(date)
-#echo "::set-output name=time::$time"
 
 #
 # Oyente
 #
-#apt install -y python3
 python3 --version
-#curl https://bootstrap.pypa.io/pip/2.7/get-pip.py --output get-pip.py
-#python2 get-pip.py
-#pip2 --version
-#apt install -y virtualenv
-
-
-#apt install -y python3
-#apt install -y ethereum
+apt install -y ethereum
 apt install -y wget
 wget https://github.com/Z3Prover/z3/archive/z3-4.5.0.tar.gz
 tar -xvzf z3-4.5.0.tar.gz
@@ -50,6 +33,21 @@ make install
 pip3 install requests
 pip3 install web3
 pip3 install crytic_compile
-cd ..
+cd ../..
 cd oyente/oyente
+pwd
 python3 oyente.py -s /github/workspace/calls.sol
+
+
+#
+# Securify
+#
+
+
+
+
+#echo "Hello $1"
+#time=$(date)
+#echo "::set-output name=time::$time"
+
+
