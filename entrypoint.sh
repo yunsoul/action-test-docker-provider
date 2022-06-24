f7ac5a0c478eb7890ddb1207ab43d7d726f80f6d
yun geun yong <happyme9@naver.com>
2022-06-24 19:45:07 +0900

    2

--- entrypoint.sh
+++ entrypoint.sh
@@ -28,8 +28,8 @@ else
                 echo "${SCAN_LIST[index]}"
         done
 fi
-TOOL_ID=0
-TOOL_NUM=1
+
+
 #
 # common
 #
@@ -41,15 +41,33 @@ sudo apt install -y solc
 wget https://raw.githubusercontent.com/yunsoul/action-test-docker-provider/main/conv
 chmod +x conv
 
+
+TOOL_NUM=2
 #
 # Mythrilp
 #
+TOOL_ID=0
 pip3 install mythril
 myth version
 for index in "${!SCAN_LIST[@]}"
 do
 	#myth analyze /github/workspace/${SCAN_LIST[index]} -o json
 	myth analyze ${SCAN_LIST[index]} -o json > "result_${TOOL_ID}_${index}.json"
+	./conv "create" $TOOL_ID "result_${TOOL_ID}_${index}.json"
+done
+index=$(( index+1 ))
+./conv "merge" $TOOL_ID $index
+
+
+
+#
+# Slither
+#
+TOOL_ID=1
+pip3 install slither-analyzer
+for index in "${!SCAN_LIST[@]}"
+do
+	slither ${SCAN_LIST[index]} --sarif "result_${TOOL_ID}_${index}.json"
 	./conv "create" $TOOL_ID "result_${TOOL_ID}_${index}.json"
 done
 index=$(( index+1 ))
